7265fb51cae931d5d8c432b93510b4185be7c497
Diy2210 <Diy2210@Mac-mini-Diy2210.local>
2016-09-26 09:55:45 +0300

    new version

--- VinDecoder/VINDecoderViewController.swift
+++ VinDecoder/VINDecoderViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class VINDecoderViewController: UIViewController {
 
-    
+    // Name labels
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var modelLabel: UILabel!
     @IBOutlet weak var cylinderLabel: UILabel!
@@ -23,6 +23,20 @@ class VINDecoderViewController: UIViewController {
     @IBOutlet weak var yearLabel: UILabel!
     @IBOutlet weak var interiorLabel: UILabel!
     @IBOutlet weak var exteriorLabel: UILabel!
+    
+    // Info labels
+    @IBOutlet weak var nameInfo: UILabel!
+    @IBOutlet weak var modelInfo: UILabel!
+    @IBOutlet weak var cylinderInfo: UILabel!
+    @IBOutlet weak var sizeInfo: UILabel!
+    @IBOutlet weak var horsepowerInfo: UILabel!
+    @IBOutlet var typeInfo: UIView!
+    @IBOutlet weak var transmissionInfo: UILabel!
+    @IBOutlet weak var wheelsInfo: UILabel!
+    @IBOutlet weak var doorsInfo: UILabel!
+    @IBOutlet weak var yearInfo: UILabel!
+    @IBOutlet weak var interiorInfo: UILabel!
+    @IBOutlet weak var exteriorInfo: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
